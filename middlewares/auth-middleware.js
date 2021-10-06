3984fca6fa18a10cc7b756672c3f287eb7544ecb
changwoo Jeong <jcw9828@gmail.com>
2021-10-06 23:02:23 +0900

    댓글작성 완료

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -3,14 +3,14 @@ const User = require("../schemas/user");
 
 module.exports = async (req, res, next) => {
   const token = req.cookies.user // 쿠키 사용법 다시 봐야함
-  // console.log(token)
+  console.log(token)
   try {
     if (token) {
-      const decoded = jwt.verify(token, "jcw-secret-key");
+      const decoded = jwt.verify(token, "jcw-secret-key"); // 토큰 대칭동기 인증
       const user = await User.findOne({ nickname: decoded.nickname }).exec()
       res.locals.user = user.nickname; // _id가 아니라 nickname으로 바꿔줘야 로그인정보 중 nickname을 가져옴
     } else {
-      res.locals.user = undefined
+      res.locals.user = undefined // locals 저장소에 있는 user
     }
   } catch (err) {
     res.status(400).send({
